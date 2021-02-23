14569072e51a98e878549203ac244e259895db88
Anurag Bansal <anuragbansaldps@gmail.com>
2021-02-23 22:07:46 +0530

    Algorithms

---
+++ Algorithms/sieve.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<algorithm>
+#include<climits>
+#include<vector>
+#include<stack>
+#include<math.h>
+#include<set>
+using namespace std;
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+       
+}
