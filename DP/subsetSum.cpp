6edab2fb2299e00b5b381cbe0a5aa7a6248d1e72
Anurag Bansal <anuragbansaldps@gmail.com>
2021-04-08 19:30:33 +0530

    subsetSum

---
+++ DP/subsetSum.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool subsetSum(int *a,int n,int sum)
+{
+    bool dp[n+1][sum+1];
+    for(int i=0;i<=sum;i++)
+        dp[0][i] = false;
+    for(int i=0;i<=n;i++)
+        dp[i][0] = true;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=sum;j++)
+        {
+            if(a[i-1]<=j)
+                dp[i][j] = dp[i-1][j-a[i-1]] || dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j]; 
+        }
+    }
+    return dp[n][sum];
+}
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int n,sum;
+    cin>>n>>sum;
+    int a[10];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    if(subsetSum(a,n,sum))
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
