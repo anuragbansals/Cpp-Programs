525ef7b58caeb6ba5541e221e8248b9c25073d96
Anurag Bansal <anuragbansaldps@gmail.com>
2021-04-28 01:22:42 +0530

    graph-algo

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -1,52 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
+template<typename T>
 
-class graph{
+class Graph{
+    map<T, list<T> > l;
     public:
-    unordered_map<int,vector<int> > map;
-    void addEdge(int x, int y)
-    {
-        map[x].push_back(y);
-        map[y].push_back(x);
+    void addEdge(int x,int y){
+        l[x].push_back(y);
+        l[y].push_back(x);
     }
-    void bfs(int src)
-    {
-        unordered_map<int, bool> visited;
-        queue<int> q;
+    void bfs(T src){
+        map<T, bool> visited;
+        queue<T> q;
         q.push(src);
         visited[src] = true;
-        while(!q.empty())
-        {
-            int k = q.front();
+        while(!q.empty()){
+            T node = q.front();
             q.pop();
-            cout<<k<<"->";
-            vector<int> :: iterator it;
-            for(auto nbr: map[k])
-            {
-                if(!visited[nbr])
+            cout<<node<<" ";
+            for(auto nbr: l[node]){
+                if(!visited[nbr]){
                     q.push(nbr);
-                visited[nbr] = true;
+                    visited[nbr] = true;
+                }
             }
         }
     }
+
 };
-
-
-
 int main()
 {
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
-    graph g;
+    Graph<int> g;
+    g.addEdge(0,1);
     g.addEdge(1,2);
-    g.addEdge(1,3);
-    g.addEdge(1,4);
     g.addEdge(2,3);
     g.addEdge(3,4);
     g.addEdge(4,5);
-    g.addEdge(5,6);
-    g.addEdge(5,7);
-    g.addEdge(5,3);
-    g.bfs(1);
+    g.bfs(0);
+
     return 0;
 }
